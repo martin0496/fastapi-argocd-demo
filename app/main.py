ac7f2d89847af1f1dc4022a15d5e2ce319284a58
martin0496 <mahdi041196@gmail.com>
2026-05-28 01:19:03 +0200

    update users and bump image to 1.1

--- app/main.py
+++ app/main.py
@@ -3,11 +3,12 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 users = {
-    "1": {"id": 1, "name": "Alice", "role": "admin"},
-    "2": {"id": 2, "name": "Bob", "role": "user"},
-    "3": {"id": 3, "name": "Charlie", "role": "guest"},
+    "1": {"id": 1, "name": "Mahdi", "role": "DevOps Engineer"},
+    "2": {"id": 2, "name": "Sara", "role": "Backend Developer"},
+    "3": {"id": 3, "name": "Youssef", "role": "Cloud Engineer"},
+    "4": {"id": 4, "name": "Lina", "role": "Frontend Developer"},
+    "5": {"id": 5, "name": "Adam", "role": "Security Analyst"},
 }
-
 @app.get("/user/{user_id}")
 def get_user(user_id: str):
     if user_id in users:
